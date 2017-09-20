0ae8165e424543b7b1131f3f1d82b76367e41874
Abdul Rahman <abdulrahman@Abduls-MacBook-Pro.local>
2017-09-20 22:12:42 +0530

    WHAT R WE?SOME KINDA SUICIDE SQUAAAADDDDDD

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-function.something(){
+
+function suicide_squad(){
+ console.log("what are we? some kinda SUICIDE SQUADDDD");
+ console.log("Nah FAMM no new characters allowed");
+};
+
+function something(){
 console.log("psych i'm nothing");
 }
+
 console.log("hello world");
 something();
+suicide_squad();
